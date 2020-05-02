548bb6b451cd024374e414ac251f4cbe494f8175
Sreekanth <sreekanth@Sreekanths-MacBook-Pro.local>
2020-05-02 17:41:07 +0530

    terraform pract

---
+++ provider.tf
@@ -0,0 +1,9 @@
+provider "azurerm" {
+    version = "=2.0.0"
+    features {}
+    
+    client_id = "bcac97dd-ebb3-4643-b853-41207d8b5f71"
+    tenant_id = "a7a647c7-be17-4792-8cd0-58a85acfdee1"
+    client_secret = "910e843b-b2e8-436d-bc12-0640dcbad9d4"
+    subscription_id = "a4bf62d4-a76f-4487-a44e-63a3dada18a8"
+}
